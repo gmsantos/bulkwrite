deb6d68331b8963c4cf93051f923e033617c3701
gmsantos <gabriel.ms1@hotmail.com>
2018-03-15 14:49:17 -0300

    Initial commit

---
+++ run-writeConcern0AsscoArray.php
@@ -0,0 +1,34 @@
+<?php
+
+$manager = new MongoDB\Driver\Manager();
+
+for ($j = 0; $j <= 40; $j++) {
+    echo "\nPrepare for bulkwrite j={$j}";
+    $bulkWrite = new MongoDB\Driver\BulkWrite(['ordered' => false]);
+
+    for ($i = 0; $i < 9000; $i++) {
+        $bulkWrite->update(
+            ['_id' => 88888888 + $i],
+            [
+                '$set' => [
+                    'name' => 'gmsantos',
+                    'counter' => $i
+                ]
+            ],
+            ['upsert' => true]
+        );
+
+        if (0 === $i % 1000 ) {
+            echo "\n {$i}...";
+        }
+    }
+
+    echo "\nExecuting bulkwrite j={$j}";
+    $manager->executeBulkWrite(
+        'db.some_collection',
+        $bulkWrite,
+        ['writeConcern' => new MongoDB\Driver\WriteConcern(0)]
+    );
+
+    echo "\nDone bulkwrite j={$j}";
+}
